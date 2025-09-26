d6158b39547283c2d54d2a4cb083dea8ae507d54
stefankosticc <skostic806@gmail.com>
2025-09-26 08:16:10 +0200

    initial commit

--- backend/artsharingapp-frontend/src/config/constants.ts
+++
@@ -1,7 +0,0 @@
-export const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_URL;
-
-export const ARTIST_FALLBACK_IMAGE =
-  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
-
-export const ARTWORK_FALLBACK_IMAGE =
-  "https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png?20210521171500";
---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by stefan on 26.9.25..
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
